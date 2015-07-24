313b3f071411bf83fa14b3089ab21fb5e69d20c6
YoshihiroUeki <pirori67@gmail.com>
2015-07-24 14:45:16 +0900

    pin set clock settings added

---
+++ frequency.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <wiringPi/wiringPi.h>
+
+int main()
+{
+  if (wiringPiSetupGpio() == -1) {
+    std::cout << "cannot setup gpio." << std::endl;
+    return 1;
+  }
+
+  pinMode(18, PWM_OUTPUT);
+  pwmSetMode(PWM_MODE_MS);
+  pwmSetClock(3000);
+  pwmSetRange(1024);
+
+  while (true) {
+    int num;
+    std::cin >> num;
+
+    if (num == -1) {
+      break;
+    }
+
+    pwmWrite(18, num);
+  }
+
+  return 0;
+}
