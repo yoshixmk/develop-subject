d2e1443791043bff92d52a6fe98490ee35448fbd
yoshihiro <pirori67@gmail.com>
2015-07-27 01:15:00 +0900

    VideoCapture sample added

--- camera_sample.cpp
+++ camera_sample.cpp
@@ -1,6 +1,7 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/videoio.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <time.h>
