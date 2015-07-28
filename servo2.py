ded14cda94dcf55762d615f5b41afae1abecb70c
YoshihiroUeki <pirori67@gmail.com>
2015-07-28 11:22:36 +0900

    servo2.py FREQ setting Update.

--- servo2.py
+++ servo2.py
@@ -18,11 +18,12 @@ for x in SERVO:
 
 start = time.time()
 
-while (time.time() - start) < 60: # Spin for 60 seconds.
-
    for x in range (len(SERVO)): # For each servo.
       pi.set_PWM_dutycycle(SERVO[x], 128)
       pi.set_PWM_frequency(SERVO[x], FREQ[x])
+
+while (time.time() - start) < 60: # Spin for 60 seconds.
+
 	  
 for x in SERVO:
    pi.set_PWM_frequency(x, 0) # Switch servo pulses off.
