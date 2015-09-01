101c5aa659fadd5ed5516d704ff857ae793a5b1a
yoshixmk_school <pirori67@gmail.com>
2015-09-01 07:29:41 +0000

    logtest modefy

--- log_output_test/log_sample_outputfile.c
+++ log_output_test/log_sample_outputfile.c
@@ -4,21 +4,14 @@ int main(void)
 {
   FILE *fp;
   char *fname = "test.txt";
-  char *str = "Hello World¥nThe C Programming Language¥nTest Test";
+  char *str = "Hello World\nThe C Programming Language\nTest Test\n";
   int i;
-
   fp = fopen( fname, "w" );
-  if( fp == NULL ){
-    printf( "%sファイルが開けません¥n", fname );
-    return -1;
-  }
 
-  for( i=0; ; i++ ){
+  for( i=0; i<100 ; i++ ){
     fputc( str[i], fp );
   }
-
   fclose( fp );
-
-  printf( "%sファイル書き込みが終わりました¥n", fname );
+  printf( "%sOK\n", fname );
   return 0;
 }
