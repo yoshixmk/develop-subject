5a34334dde3d059c31870aa9bff57f922b05cb88
植木善弘 <pirori67@gmail.com>
2015-09-02 23:53:44 +0900

    new file:   camera/photo_save_capture.cpp

---
+++ camera/photo_save_capture.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int
+main(int argc, char *argv[])
+{
+  cv::VideoCapture cap(0);
+  // 様々な設定...
+  cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+  cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+  // カメラがオープンできたかの確認
+  if(!cap.isOpened()) return -1;
+
+  cv::namedWindow("Capture", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
+  while(1) {
+    cv::Mat frame;
+    cap >> frame;  // キャプチャ
+    // 様々な処理
+    // ...
+    cv::imshow("Capture", frame); // 表示
+    if(cv::waitKey(30) >= 0)
+       {
+    cv::imwrite("photodir/cap.png", frame);
+       break;
+       }
+  }
+}
