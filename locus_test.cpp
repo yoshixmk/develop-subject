d77013a998a84f7e05bde1a85bd6a05ee4352861
yoshixmk_school <pirori67@gmail.com>
2015-09-07 06:14:28 +0000

    comment deleted. source cleanup

--- locus_test.cpp
+++ locus_test.cpp
@@ -1,36 +1,3 @@
-// Air Hockey Robot project
-// Computer Vision for PS3 EYE Camera (OpenCV)
-// Author: Jose Julio
-// Date: Dec 2013 - Jan 2014
-
-// This code needs the OpenCV libraries
-
-// This code detects the puck and the robot in the hockey game at 60Hz
-// Detects two objects PUCK and ROBOT with two different colors
-// And sends serial packets to arduino at 115200 bauds
-// Serial packet:
-//     Sync start: 2 bytes: mm
-//     TimeStamp: 2 bytes
-//     Pos_X:     2 bytes (0-640)
-//     Pos_Y:     2 bytes (0-480)
-//     Object size:     2 bytes (0-400)
-//     Robot Pos_X:     2 bytes (0-640)
-//     Robot Pos_Y:     2 bytes (0-480)
-
-// Steps:
-// Capture a frame, convert to HSV,
-// For both PUCK and ROBOT:
-//    threshold based on HSV range, Object extraction, roundness and size filter, Center calculation
-// For the PUCK we make a trajectory prediction to draw the lines...
-// We record a video in the file output.mpeg and a log file with the received serial messages from robot
-
-// H : 70-94 S: 60-150 V: 10-145   GREEN EVA FOAM
-// H : 5-20  S: 110-200 V: 90-200  ORANGE EVA FOAM
-
-// Default parameters are for a GREEN EVA FOAM PUCK and an ORANGE EVA FOAM ROBOT MARK
-
-// PS3 Camera windows driver from : http://codelaboratories.com/products/eye/driver/
-
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <time.h>
@@ -41,8 +8,6 @@
 #define robot_center_x 300   // Center of robot
 #define robot_center_y 500
 
-// PS3 Camera pixels
-// NOTE: We are using the camera at 320x240 but we are outputing a 640x480 pixel position
 #define CAM_PIX_WIDTH 640
 #define CAM_PIX_HEIGHT 480
 #define CAM_PIX_TO_MM 1.4
@@ -117,143 +82,6 @@ int cam_center_y;
 float cam_pix_to_mm;
 float cam_rotation;  //Camera rotation in radians
 
-
-// Camera process, convert puck position to coordinates and generate trajectory prediction and visualization
-// Simple lens distortion correction (one parameter) NOT USED NOW
-// Xu = (Xd)/(1+param*dist2)  dist2 = distancia al cuadrado del pixel al centro
-void cameraProcess(int PosX, int PosY, int time)
-{
-  int coordX;
-  int coordY;
-  int vectorX;
-  int vectorY;
-  double slope;
-
-  int bounce_x;
-  int bounce_y;
-  int predict_pixX;
-  int predict_pixY;
-  int bounce_pixX;
-  int bounce_pixY;
-
-  // Convert from Camera reference system to Robot reference system
-  // We suppose very small angle rotatios (less than 5 degrees) so we use the
-  // aproximation that sin cam_rotation = cam_rotation (in radians)
-  // Camera X axis correspond to robot Y axis
-  coordY = (posX - cam_center_x);   // First we convert image coordinates to center of image
-  coordX = (posY - cam_center_y);
-
-  coordY = robot_center_y - coordY*cam_pix_to_mm;
-  coordX = robot_center_x - coordX*cam_pix_to_mm*(1-cam_rotation);
-
-  // Calculate speed and angle
-  vectorX = (coordX-puckCoordX);
-  vectorY = (coordY-puckCoordY);
-
-  puckSpeedX = vectorX*100/time;  // speed in dm/ms (
-  puckSpeedY = vectorY*100/time;
-  //puckSpeed = sqrt(vectorX*vectorX + vectorY*vectorY)*1000.0/time;
-  //puckDirection = atan2(vectorY,vectorX);
-  puckOldCoordX = puckCoordX;
-  puckOldCoordY = puckCoordY;
-  puckCoordX = coordX;
-  puckCoordY = coordY;
-
-  // Noise detection, big vector...
-  if ((vectorY<-100)||(vectorY>100)||(vectorX>100)||(vectorX<-100))
-  {
-    sprintf(tempStr2,"NOISE %d",vectorY);
-    return;
-  }
-
-  // Its time to predict...
-  // Based on actual position, speed and angle we need to know the future...
-  // Posible impact?
-  if (puckSpeedY<-50)
-    {
-      // Puck is comming...
-      // We need to predict the puck position when it reaches our goal Y=0
-      // slope formula: m = (y2-y1)/(x2-x1)
-    if (vectorX == 0)  // To avoid division by 0
-    slope = 9999999;
-    else
-    slope = (float)vectorY/(float)vectorX;
-      // x = (y2-y1)/m + x1
-    predict_y = defense_position;
-    predict_x = (predict_y-coordY)/slope + coordX;
-
-    // puck has a bounce with side wall?
-      if ((predict_x<38)||(predict_x>562))
-        {
-    // We start a new prediction
-    // Wich side?
-    if (predict_x<38)
-      {
-      //Left side. We calculare the impact point
-      bounce_x = 38;
-      }
-    else
-      {
-      //Right side. We calculare the impact point
-      bounce_x = 562;
-      }
-    bounce_y = (bounce_x - coordX)*slope + coordY;
-    bounce_pixX = cam_center_x - (bounce_y-robot_center_y)/cam_pix_to_mm;
-    bounce_pixY = cam_center_y - (bounce_x-robot_center_x)/(cam_pix_to_mm*(1-cam_rotation));
-    predict_time = (bounce_y-puckCoordY)*100/puckSpeedY;  // time until bouce
-    // bounce prediction
-    // Slope change
-    slope = -slope;
-    predict_y = defense_position;
-    predict_x = (predict_y-bounce_y)/slope + bounce_x;
-
-    if ((predict_x<38)||(predict_x>562))
-      {
-      // New bounce??
-      // We do nothing then...
-      sprintf(tempStr2,"2B %d %d",bounce_x,bounce_y);
-      predict_x_old = -1;
-      }
-    else
-      {
-      // draw line
-      cvLine(img, cvPoint(posX/2, posY/2), cvPoint(bounce_pixX/2, bounce_pixY/2), cvScalar(255,0,0), 2);
-      // result average
-      if (predict_x_old != -1)
-        predict_x = (predict_x_old + predict_x)>>1;
-      predict_x_old = predict_x;
-      predict_time = predict_time + (predict_y-puckCoordY)*100/puckSpeedY;  // in ms
-      sprintf(tempStr2,"%d;%d %d %d t%d",coordX,coordY,predict_x,puckSpeedY,predict_time);
-      predict_pixX = cam_center_x - (predict_y-robot_center_y)/cam_pix_to_mm;
-      predict_pixY = cam_center_y - (predict_x-robot_center_x)/(cam_pix_to_mm*(1-cam_rotation));
-      // draw line
-      cvLine(img, cvPoint(bounce_pixX/2, bounce_pixY/2), cvPoint(predict_pixX/2, predict_pixY/2), cvScalar(0,255,0), 2);
-      }
-     }
-      else  // No bounce, direct impact
-        {
-    // result average
-    if (predict_x_old != -1)
-      predict_x = (predict_x_old + predict_x)>>1;
-    predict_x_old = predict_x;
-
-    predict_time = (predict_y-puckCoordY)*100/puckSpeedY;  // in ms
-    sprintf(tempStr2,"%d;%d %d %d t%d",coordX,coordY,predict_x,puckSpeedY,predict_time);
-    // Convert impact prediction position to pixels (to show on image)
-    predict_pixX = cam_center_x - (predict_y-robot_center_y)/cam_pix_to_mm;
-    predict_pixY = cam_center_y - (predict_x-robot_center_x)/(cam_pix_to_mm*(1-cam_rotation));
-    // draw line
-    cvLine(img, cvPoint(posX/2, posY/2), cvPoint(predict_pixX/2, predict_pixY/2), cvScalar(0,255,0), 2);
-        }
-    }
-  else // Puck is moving slowly or to the other side
-  {
-  sprintf(tempStr2,"NO %d,%d %d",coordX,coordY,puckSpeedY);
-  predict_x_old = -1;
-  }
-
-}
-
 void cameraProcessInit()
 {
     // Default values
@@ -265,126 +93,11 @@ void cameraProcessInit()
   defense_position = 100;  // Pusher defense position
 }
 
-
 //This function threshold the HSV image and create a binary image
 IplImage* GetThresholdedImage(IplImage* imgHSV, int minH, int maxH, int minS, int maxS, int minV, int maxV){
     IplImage* imgThresh=cvCreateImage(cvGetSize(imgHSV),IPL_DEPTH_8U, 1);
     cvInRangeS(imgHSV, cvScalar(minH,minS,minV), cvScalar(maxH,maxS,maxV), imgThresh);
     return imgThresh;
-}
-
-
-// Image segmentation, object extraction based on size and form (roundness)
-void trackObjectPuck(IplImage* imgThresh){
-
- CvSeq* contours;  //hold the pointer to a contour in the memory block
- CvSeq* result;   //hold sequence of points of a contour
- CvMemStorage *storage = cvCreateMemStorage(0); //storage area for all contours
-
- // Position initialization
- posX = 0;
- posY = 0;
- status = 0;
-
- //finding all contours in the image (segmentation)
- cvFindContours(imgThresh, storage, &contours, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
- if (contours)
-  status=1;
- while (contours)
- {
-   double area = cvContourArea(contours);
-
-   if ((area>20)&&(area<400))  // Min and Max size of object
-   {
-    status = 2;
-        //Detecting roundness   roundness = perimeter2/(2*pi*area)
-    double perimeter = cvArcLength(contours);
-    double roundness = (perimeter*perimeter)/(6.28*area);
-    //printf("%lf",roundness);
-    if (roundness < 8)
-    {
-      status = 3;
-      CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));
-      cvMoments(contours, moments, 1);
-      double moment10 = cvGetSpatialMoment(moments, 1, 0);
-      double moment01 = cvGetSpatialMoment(moments, 0, 1);
-      area = cvGetCentralMoment(moments, 0, 0);
-
-      // Calculate object center
-      // We are using 320x240 pix but we are going to output the 640x480 equivalent (*2)
-      posX = floor(moment10*2/(double)area+0.5); // round
-      posY = floor(moment01*2/(double)area+0.5);
-      objectSize = area;
-      if(lastX>=0 && lastY>=0 && posX>=0 && posY>=0)
-      {
-        status = 4;
-        // Draw a line from the previous point to the current point
-        cvLine(img, cvPoint(posX/2, posY/2), cvPoint(lastX/2, lastY/2), cvScalar(255,255,0), 4);
-        // Draw contour
-        cvDrawContours(img,contours,cvScalar(255,0,0),cvScalar(0,0,255),0,1,8,cvPoint(0,0));
-      }
-      lastX = posX;
-      lastY = posY;
-    }
-   }
-
-   //obtain the next contour
-     contours = contours->h_next;
- }
- //printf("\n");
- cvReleaseMemStorage(&storage);
-}
-
-// Image segmentation, object extraction based on size and form (roundness)
-void trackObjectRobot(IplImage* imgThresh){
-
- CvSeq* contours;  //hold the pointer to a contour in the memory block
- CvSeq* result;   //hold sequence of points of a contour
- CvMemStorage *storage = cvCreateMemStorage(0); //storage area for all contours
-
- // Position initialization
- RposX = 0;
- RposY = 0;
- RobjectSize = 0;
-
- //finding all contours in the image (segmentation)
- cvFindContours(imgThresh, storage, &contours, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
- while (contours)
- {
-   double area = cvContourArea(contours);
-
-   if ((area>18)&&(area<100))  // Min and Max size of object
-   {
-        //Detecting roundness   roundness = perimeter2/(2*pi*area)
-    double perimeter = cvArcLength(contours);
-    double roundness = (perimeter*perimeter)/(6.28*area);
-    //printf("%lf",roundness);
-    if (roundness < 4)
-    {
-      //printf("area:%.2lf R:%.2lf\n",area,roundness);
-      CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));
-      cvMoments(contours, moments, 1);
-      double moment10 = cvGetSpatialMoment(moments, 1, 0);
-      double moment01 = cvGetSpatialMoment(moments, 0, 1);
-      area = cvGetCentralMoment(moments, 0, 0);
-
-      // Calculate object center
-      // We are using 320x240 pix but we are going to output the 640x480 equivalent (*2)
-      RposX = floor(moment10*2/(double)area+0.5); // round
-      RposY = floor(moment01*2/(double)area+0.5);
-      RobjectSize = area;
-      if(RposX>=0 && RposY>=0)
-      {
-        cvLine(img, cvPoint(RposX/2, RposY/2), cvPoint(RposX/2, RposY/2), cvScalar(100,255,50), 4);
-        cvDrawContours(img,contours,cvScalar(0,0,100),cvScalar(100,0,0),0,1,8,cvPoint(0,0));
-      }
-    }
-   }
-
-   //obtain the next contour
-     contours = contours->h_next;
- }
- cvReleaseMemStorage(&storage);
 }
 
 int main(int argc, char* argv[]){
@@ -393,7 +106,6 @@ int main(int argc, char* argv[]){
 
   printf("start!\n");
 
-  // SHOW parameters:
   printf("PUCK MINH: %d\n",minH);
   printf("PUCK MAXH: %d\n",maxH);
   printf("PUCK MINS: %d\n",minS);
@@ -447,13 +159,6 @@ cvNamedWindow("circle_sample2", CV_WINDOW_AUTOSIZE);
     /// Apply the erosion-dilatation operation for filtering
     cvErode( imgThresh, imgThresh, NULL,1 );
     cvDilate( imgThresh, imgThresh, NULL,1 );
-
-    //track the possition of the puck and the robot
-    //trackObjectPuck(imgThresh);
-    //trackObjectRobot(imgThresh2);
-
-    // TEST CAMERA PROCESS
-    //cameraProcess(posX,posY,16.66);
     
  CvSeq* contours;  //hold the pointer to a contour in the memory block
  CvSeq* result;   //hold sequence of points of a contour
@@ -467,13 +172,10 @@ cvNamedWindow("circle_sample2", CV_WINDOW_AUTOSIZE);
  cvFindContours(imgThresh, storage, &contours, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
   IplImage* dst_img;
   dst_img = cvCreateImage(cvSize(384, 512), IPL_DEPTH_8U, 3);
- // cvRepeat(img, dst_img);
  cvDrawContours(img, contours, CV_RGB(255,0,0), CV_RGB(255,0,0), 0, 2);
-    //cvLine endpoint -> startpoint ordering
     cvLine(img, cvPoint(10, 300), cvPoint(50, 50), cvScalar(0,255,0), 2);
     cvPutText (img, tempStr2, cvPoint(150,20), &font, cvScalar(255,255,0));
 
-    // LOG TEXT
     cvPutText (img, logStr, cvPoint(20,220), &font, cvScalar(50,220,220));
 
     cvShowImage("Video", img);
@@ -495,21 +197,9 @@ cvNamedWindow("circle_sample2", CV_WINDOW_AUTOSIZE);
     cvReleaseImage(&imgThresh2);
     cvReleaseImage(&img);
 
-    //Wait 1mS necesary???
-    // int c = cvWaitKey(1);
-    //If 'ESC' is pressed, break the loop
-    // if((char)c==27 ) break;
-
-    //counter++;
-    //}
-
-
     cvDestroyAllWindows() ;
     cvReleaseImage(&imgTracking);
-    // cvReleaseCapture(&capture);
   cvReleaseVideoWriter(&record);
-
-  // fclose(logFile);
 
     return 0;
 }
