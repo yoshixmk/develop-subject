5325b4d944fb89f72779bb164536f1f9fc896aa0
yoshixmk_school <pirori67@gmail.com>
2015-10-14 01:01:16 +0000

    temperature/temp_hum_eon.py added.

--- temperature/temp_hum_eon.py
+++ temperature/temp_hum_eon.py
@@ -1,7 +1,17 @@
-import sys
+# -*- coding: utf-8 -*-
+import os
 import time
+import sys
 import RPi.GPIO as GPIO
+from pubnub import Pubnub
 
+pubnub = Pubnub(publish_key='pub-c-35c23189-77f7-417a-967f-e00401cce82a', subscribe_key='sub-c-96d9098e-7064-11e5-98e8-0619f8945a4f')
+channel = 'tempeon'
+
+def callback(message):
+    print(message)
+
+#published in this fashion to comply with Eon
 spi_clk  = 11
 spi_mosi = 10
 spi_miso = 9
@@ -14,10 +24,7 @@ GPIO.setup(spi_mosi, GPIO.OUT)
 GPIO.setup(spi_miso, GPIO.IN)
 GPIO.setup(spi_clk,  GPIO.OUT)
 GPIO.setup(spi_ss,   GPIO.OUT)
-
 while True:
-    time.sleep(0.1)
-
     GPIO.output(spi_ss,   False)
     GPIO.output(spi_clk,  False)
     GPIO.output(spi_mosi, False)
@@ -50,3 +57,12 @@ while True:
     sys.stdout.write(str(value))
 
     sys.stdout.write("\n")
+
+    print 'Temp={0:0.1f}*C'.format(value)
+    pubnub.publish('tempeon', {
+        'columns': [
+            ['x', time.time()],
+            ['temperature_celcius', value]
+            ]
+	    })
+
