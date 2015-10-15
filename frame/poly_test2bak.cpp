236871f5d7ce0f9544d82ca9aa3dd825fc85e486
yoshixmk_school <pirori67@gmail.com>
2015-10-15 05:00:20 +0000

    poly detected. labeling -> poly

--- frame/poly_test2bak.cpp
+++ frame/poly_test2bak.cpp
@@ -167,18 +167,191 @@ void cv_Labelling(
 
 
 
+//---------------------------------------------------------------
+//【関数名　】：cv_ColorExtraction
+//【処理概要】：色抽出
+//【引数　　】：src_img        = 入力画像(8bit3ch)
+//　　　　　　：dst_img        = 出力画像(8bit3ch)
+//　　　　　　：code        = 色空間の指定（CV_BGR2HSV,CV_BGR2Labなど）
+//　　　　　　：ch1_lower    = ch1のしきい値(小)
+//　　　　　　：ch1_upper    = ch1のしきい値(大)
+//　　　　　　：ch2_lower    = ch2のしきい値(小)
+//　　　　　　：ch2_upper    = ch2のしきい値(大)
+//　　　　　　：ch3_lower    = ch3のしきい値(小)
+//　　　　　　：ch3_upper    = ch3のしきい値(大)
+//【戻り値　】：なし
+//【備考　　】：lower <= upperの場合、lower以上upper以下の範囲を抽出、
+//　　　　　　：lower >  upperの場合、upper以下lower以上の範囲を抽出します。
+//---------------------------------------------------------------
+void cv_ColorExtraction(IplImage* src_img, IplImage* dst_img,
+                        int code,
+                        int ch1_lower, int ch1_upper,
+                        int ch2_lower, int ch2_upper,
+                        int ch3_lower, int ch3_upper
+                       ) {
+
+    int i, k;
+
+    IplImage *Color_img;
+    IplImage *ch1_img, *ch2_img, *ch3_img;
+    IplImage *Mask_img;
+
+    int lower[3];
+    int upper[3];
+    int val[3];
+
+    CvMat *lut;
+
+    //codeに基づいたカラー変換
+    Color_img = cvCreateImage(cvGetSize(src_img), src_img->depth, src_img->nChannels);
+    cvCvtColor(src_img, Color_img, code);
+
+    //3ChのLUT作成
+    lut    = cvCreateMat(256, 1, CV_8UC3);
+
+    lower[0] = ch1_lower;
+    lower[1] = ch2_lower;
+    lower[2] = ch3_lower;
+
+    upper[0] = ch1_upper;
+    upper[1] = ch2_upper;
+    upper[2] = ch3_upper;
+
+    for (i = 0; i < 256; i++) {
+        for (k = 0; k < 3; k++) {
+            if (lower[k] <= upper[k]) {
+                if ((lower[k] <= i) && (i <= upper[k])) {
+                    val[k] = 255;
+                } else {
+                    val[k] = 0;
+                }
+            } else {
+                if ((i <= upper[k]) || (lower[k] <= i)) {
+                    val[k] = 255;
+                } else {
+                    val[k] = 0;
+                }
+            }
+        }
+        //LUTの設定
+        cvSet1D(lut, i, cvScalar(val[0], val[1], val[2]));
+    }
+
+    //3ChごとのLUT変換（各チャンネルごとに２値化処理）
+    cvLUT(Color_img, Color_img, lut);
+    cvReleaseMat(&lut);
+
+    //各チャンネルごとのIplImageを確保する
+    ch1_img = cvCreateImage(cvGetSize(Color_img), Color_img->depth, 1);
+    ch2_img = cvCreateImage(cvGetSize(Color_img), Color_img->depth, 1);
+    ch3_img = cvCreateImage(cvGetSize(Color_img), Color_img->depth, 1);
+
+    //チャンネルごとに二値化された画像をそれぞれのチャンネルに分解する
+    cvSplit(Color_img, ch1_img, ch2_img, ch3_img, NULL);
+
+    //3Ch全てのANDを取り、マスク画像を作成する。
+    Mask_img = cvCreateImage(cvGetSize(Color_img), Color_img->depth, 1);
+    cvAnd(ch1_img, ch2_img, Mask_img);
+    cvAnd(Mask_img, ch3_img, Mask_img);
+
+    //入力画像(src_img)のマスク領域を出力画像(dst_img)へコピーする
+    cvZero(dst_img);
+    cvCopy(src_img, dst_img, Mask_img);
+
+    //解放
+    cvReleaseImage(&Color_img);
+    cvReleaseImage(&ch1_img);
+    cvReleaseImage(&ch2_img);
+    cvReleaseImage(&ch3_img);
+    cvReleaseImage(&Mask_img);
+
+}
+
+/* コールバック関数 */
+/*void
+on_mouse (int event, int x, int y, int flags, void *param = NULL)
+{
+  char str[64];
+  static int line = 0;
+  const int max_line = 15, w = 15, h = 30;
+
+  // (4)マウスイベントを取得
+  switch (event) {
+  case CV_EVENT_MOUSEMOVE:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "MOUSE_MOVE");
+    break;
+  case CV_EVENT_LBUTTONDOWN:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "LBUTTON_DOWN");
+    break;
+  case CV_EVENT_RBUTTONDOWN:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "RBUTTON_DOWN");
+    break;
+  case CV_EVENT_MBUTTONDOWN:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "MBUTTON_DOWN");
+    break;
+  case CV_EVENT_LBUTTONUP:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "LBUTTON_UP");
+    break;
+  case CV_EVENT_RBUTTONUP:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "RBUTTON_UP");
+    break;
+  case CV_EVENT_MBUTTONUP:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "MBUTTON_UP");
+    break;
+  case CV_EVENT_LBUTTONDBLCLK:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "LBUTTON_DOUBLE_CLICK");
+    break;
+  case CV_EVENT_RBUTTONDBLCLK:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "RBUTTON_DOUBLE_CLICK");
+    break;
+  case CV_EVENT_MBUTTONDBLCLK:
+    snprintf (str, 64, "(%d,%d) %s", x, y, "MBUTTON_DOUBLE_CLICK");
+    break;
+  }
+
+  // (5)マウスボタン，修飾キーを取得
+  if (flags & CV_EVENT_FLAG_LBUTTON)
+    strncat (str, " + LBUTTON", 64);
+  if (flags & CV_EVENT_FLAG_RBUTTON)
+    strncat (str, " + RBUTTON", 64);
+  if (flags & CV_EVENT_FLAG_MBUTTON)
+    strncat (str, " + MBUTTON", 64);
+  if (flags & CV_EVENT_FLAG_CTRLKEY)
+    strncat (str, " + CTRL", 64);
+  if (flags & CV_EVENT_FLAG_SHIFTKEY)
+    strncat (str, " + SHIFT", 64);
+  if (flags & CV_EVENT_FLAG_ALTKEY)
+    strncat (str, " + ALT", 64);
+
+  // (6)マウス座標，イベント，修飾キーなどを画像に描画，表示
+  if (line > max_line) {
+    cvGetRectSubPix (img, img, cvPoint2D32f (320 - 0.5, 240 - 0.5 + h));
+    cvPutText (img, str, cvPoint (w, 20 + h * max_line), &font, CV_RGB (0, 200, 100));
+  }
+  else {
+    cvPutText (img, str, cvPoint (w, 20 + h * line), &font, CV_RGB (0, 200, 100));
+  }
+  line++;
+  cvShowImage ("Image", img);
+}*/
+
+
 int main(int argc, char* argv[])
 {
     IplImage *src_img, *dst_img;
 
     // (1)画像を読み込む
-    if ((src_img = cvLoadImage ("../camera/photodir/capmalletf.png", CV_LOAD_IMAGE_GRAYSCALE)) == 0)
-        return -1;
+    /*if ((src_img = cvLoadImage ("../camera/photodir/capmallet1.png", CV_LOAD_IMAGE_GRAYSCALE)) == 0)
+        return -1;*/
+    
     if ((dst_img = cvLoadImage ("../camera/photodir/capmalletf.png", CV_LOAD_IMAGE_COLOR )) == 0)
         return -1;
-
+    src_img = cvCreateImage(cvGetSize(dst_img), IPL_DEPTH_8U, 1);
+    IplImage* dst_img_pack = cvCreateImage(cvGetSize(dst_img), IPL_DEPTH_8U, 3);
+	cv_ColorExtraction(dst_img, dst_img_pack, CV_BGR2HSV, 0, 54, 77, 255, 0, 255);
+	
+	cvCvtColor(dst_img_pack, src_img, CV_BGR2GRAY);
     cv_Labelling(src_img, dst_img);
-
 	cv::Mat eigen_img = cv::cvarrToMat(dst_img);  // データをコピーする
 
 	Mat gray_img;
@@ -189,11 +362,11 @@ int main(int argc, char* argv[])
     goodFeaturesToTrack(gray_img, corners, 80, 0.01, 5);
     vector<Point2f>::iterator it_corner = corners.begin();
     for(; it_corner!=corners.end(); ++it_corner) {
-        if((it_corner->y < 76 || 345 < it_corner->y) && (it_corner->x < 75 || 690 < it_corner->x) ){
+        //if((it_corner->y < 76 || 345 < it_corner->y) && (it_corner->x < 75 || 690 < it_corner->x) ){
             circle(eigen_img, Point(it_corner->x, it_corner->y), 1, Scalar(0,200,255), -1);
             circle(eigen_img, Point(it_corner->x, it_corner->y), 8, Scalar(0,200,255));
             std::cout << "x:" << it_corner->x << ", y:" << it_corner->y  << std::endl;
-        }
+        //}
     }
 
     cvNamedWindow ("Image", CV_WINDOW_AUTOSIZE);
