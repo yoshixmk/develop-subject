0d60c665b61da16defbc0d3af36136cf35ce565b
yoshixmk_school <pirori67@gmail.com>
2015-10-16 07:50:52 +0000

    merge frame detected.

--- pack_mallett_frame.cpp
+++ pack_mallett_frame.cpp
@@ -11,32 +11,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-// All units in milimeters
-#define robot_center_x 300   // Center of robot
-#define robot_center_y 500
-
 #define CAM_PIX_2HEIGHT (2 * CAM_PIX_HEIGHT)
 #define CAM_PIX_WIDTH  160
 #define CAM_PIX_HEIGHT 120
-#define CAM_PIX_TO_MM 1.4
-
-time_t start,end;
-
-int fps=30;
-
-// OpenCV variables
-CvFont font;
-IplImage* imgTracking;
-IplImage* imgThresh;
-IplImage* imgThresh2;
-IplImage* frame=0;
-
-//This function threshold the HSV image and create a binary image
-IplImage* GetThresholdedImage(IplImage* imgHSV, int minH, int maxH, int minS, int maxS, int minV, int maxV) {
-    IplImage* imgThresh=cvCreateImage(cvGetSize(imgHSV),IPL_DEPTH_8U, 1);
-    cvInRangeS(imgHSV, cvScalar(minH,minS,minV), cvScalar(maxH,maxS,maxV), imgThresh);
-    return imgThresh;
-}
 
 void DrawNextContour(
     IplImage *img,    //ﾗﾍﾞﾘﾝｸﾞ結果を描画するIplImage(8Bit3chｶﾗｰ)
@@ -302,14 +279,24 @@ const char* to_c_char(int val)
 	return stream.str().c_str();
 }
 
-//タイマー制御用関数。時間が経ったらリセット
-void pwmReset(void){
-	gpioWrite(25, 0);
-	//gpioSetPWMfrequency(25, 0);
-	gpioSetTimerFunc(0, 6000, NULL);
-}
-
 int main(int argc, char* argv[]) {
+	IplImage *src_img, *dst_img, *poly_dst, *poly_tmp, *poly_gray, *ipl_concat;
+    CvMemStorage *contStorage = cvCreateMemStorage(0);
+    CvSeq *contours;
+    CvTreeNodeIterator polyIterator;
+    
+    int found = 0;
+    int i;
+    CvPoint poly_point;
+	int fps=30;
+	
+	// ポリライン近似
+    CvMemStorage *polyStorage = cvCreateMemStorage(0);
+    CvSeq *polys, *poly;
+
+	// OpenCV variables
+	CvFont font;
+	
     printf("start!\n");
 
 	//pwm initialize
@@ -317,11 +304,22 @@ int main(int argc, char* argv[]) {
 	//pigpio CW/CCW pin setup
 	//X:18, Y1:14, Y2:15
 	gpioSetMode(18, PI_OUTPUT);
+	gpioSetMode(14, PI_OUTPUT);
+	gpioSetMode(15, PI_OUTPUT);
 	//pigpio pulse setup
 	//X:25, Y1:23, Y2:24
 	gpioSetMode(25, PI_OUTPUT);
+	gpioSetMode(23, PI_OUTPUT);
+	gpioSetMode(24, PI_OUTPUT);
 	//limit-switch setup
+	gpioSetMode(5, PI_INPUT);
+	gpioWrite(5, 0);
+	gpioSetMode(6, PI_INPUT);
+	gpioWrite(6, 0);
 	gpioSetMode(13, PI_INPUT);
+	gpioSetMode(19, PI_INPUT);
+	gpioSetMode(26, PI_INPUT);
+	gpioSetMode(21, PI_INPUT);
  
 	CvCapture* capture_robot_side;
 	CvCapture* capture_human_side;
@@ -341,8 +339,6 @@ int main(int argc, char* argv[]) {
 	cvNamedWindow("Now Image", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("pack", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("mallett", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("Vertical Concat", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow ("Outer Frame", CV_WINDOW_AUTOSIZE);
 	
 	//Create trackbar to change brightness
 	int iSliderValue1 = 50;
@@ -356,7 +352,7 @@ int main(int argc, char* argv[]) {
 	int iSliderValuePack2 = 84;//106;
 	cvCreateTrackbar("maxH", "pack", &iSliderValuePack2, 255);
 	int iSliderValuePack3 = 81;//219;
-	cvCreateTrackbar("minS", "pack", &iSliderValuePack2, 255);
+	cvCreateTrackbar("minS", "pack", &iSliderValuePack3, 255);
 	int iSliderValuePack4 = 175;//255;
 	cvCreateTrackbar("maxS", "pack", &iSliderValuePack4, 255);
 	int iSliderValuePack5 = 0;//29;
@@ -381,9 +377,8 @@ int main(int argc, char* argv[]) {
 	IplImage* img_robot_side = cvQueryFrame(capture_robot_side);
 	IplImage* img_human_side = cvQueryFrame(capture_human_side);
 	IplImage* img_all_round = cvCreateImage(cvSize(CAM_PIX_WIDTH, CAM_PIX_2HEIGHT), IPL_DEPTH_8U, 3);
-	IplImage* img2  = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
+	IplImage* img_all_round2  = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
 	IplImage* show_img  = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
-		
 	//IplImage* -> Mat
 	cv::Mat pre_src;
 	cv::Mat pre_dst;
@@ -392,8 +387,9 @@ int main(int argc, char* argv[]) {
 	double dContrast = iSliderValue2 / 50.0;
 	pre_src.convertTo(pre_dst, -1, dContrast, iBrightness); 
 	//明るさ調整した結果を変換(Mat->IplImage*)して渡す。その後解放。
-	*img_robot_side = pre_dst;
+	*img_robot_side = pre_dst.clone();
 	pre_src.release();
+	pre_dst.release();
 	
 	printf("redy?\n");
 	//決定ボタンが押されたらスタート
@@ -405,67 +401,102 @@ int main(int argc, char* argv[]) {
 	}*/
 	printf("go!\n");
 	
+	cv::Mat dst_bright_cont;
 	cv::Mat mat_frame1;
 	cv::Mat mat_frame2;
 	cv::Mat dst_img_v;
-	cv::Mat dst_bright_cont;
 	int rotate_times = 0;
+	//IplImage* -> Mat
+	mat_frame1 = cv::cvarrToMat(img_robot_side);
+	mat_frame2 = cv::cvarrToMat(img_human_side);
+	//上下左右を反転。本番環境では、mat_frame1を反転させる
+	cv::flip(mat_frame2, mat_frame2, 0); //水平軸で反転（垂直反転）
+	cv::flip(mat_frame2, mat_frame2, 1); //垂直軸で反転（水平反転）
+	vconcat(mat_frame2, mat_frame1, dst_img_v);
+	
+	iBrightness  = iSliderValue1 - 50;
+	dContrast = iSliderValue2 / 50.0;
+	dst_img_v.convertTo(dst_bright_cont, -1, dContrast, iBrightness); //１枚にした画像をコンバート
+	//明るさ調整した結果を変換(Mat->IplImage*)して渡す。その後解放。
+	*img_all_round = dst_bright_cont;
+	src_img = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 1);
+    IplImage* dst_img_pack = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
+	cv_ColorExtraction(img_all_round, dst_img_pack, CV_BGR2HSV, 0, 54, 77, 255, 0, 255);
+	
+	cvCvtColor(dst_img_pack, src_img, CV_BGR2GRAY);
+    cv_Labelling(src_img, img_all_round);
+
+    poly_gray = cvCreateImage( cvGetSize(img_all_round),IPL_DEPTH_8U,1);
+    cvCvtColor(img_all_round, poly_gray, CV_BGR2GRAY);
+	printf("go!\n");
+    poly_tmp = cvCreateImage( cvGetSize( poly_gray), IPL_DEPTH_8U, 1);
+    poly_dst = cvCreateImage( cvGetSize( poly_gray), IPL_DEPTH_8U, 3);
+    cvCopy( poly_gray, poly_tmp);
+    cvCvtColor( poly_gray, poly_dst, CV_GRAY2BGR);
+    
+	// 輪郭抽出
+    found = cvFindContours( poly_tmp, contStorage, &contours, sizeof( CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+    // ポリライン近似
+    polys = cvApproxPoly( contours, sizeof( CvContour), polyStorage, CV_POLY_APPROX_DP, 8, 10);
+    
+    cvInitTreeNodeIterator( &polyIterator, ( void*)polys, 10);
+    while( (poly = (CvSeq *)cvNextTreeNode( &polyIterator)) != NULL)
+    {
+            for( i=0; i<poly->total; i++)
+            {
+                poly_point = *( CvPoint*)cvGetSeqElem( poly, i);
+                cvCircle( poly_dst, poly_point, 1, CV_RGB(255, 0 , 255), -1);
+                cvCircle( poly_dst, poly_point, 8, CV_RGB(255, 0 , 255));
+                std::cout << "x:" << poly_point.x << ", y:" << poly_point.y  << std::endl;
+            }
+    }
+    cvNamedWindow ("Image", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow ("Poly", CV_WINDOW_AUTOSIZE);
+    
+    while(1){
+		cvShowImage ("Poly", poly_dst);
+		cvShowImage ("Image", img_all_round);
+        if(cv::waitKey(30) >= 0) {
+            break;
+        }
+	}
+	
+	// Init font
+	cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX|CV_FONT_ITALIC, 0.4,0.4,0,1);
 	while(1){
-		img2 = cvCloneImage(img_all_round);
-		show_img = cvCloneImage(img_all_round);
+		cvCopy(img_all_round, img_all_round2);
+		cvCopy(img_all_round, show_img);
+		//img_all_round2 = cvCloneImage(img_all_round);
+		//show_img = cvCloneImage(img_all_round);
 		img_robot_side = cvQueryFrame(capture_robot_side);
 		img_human_side = cvQueryFrame(capture_human_side);
 		//IplImage* -> Mat
-		cv::Mat mat_frame1 = cv::cvarrToMat(img_robot_side);
-		cv::Mat mat_frame2 = cv::cvarrToMat(img_human_side);
+		mat_frame1 = cv::cvarrToMat(img_robot_side);
+		mat_frame2 = cv::cvarrToMat(img_human_side);
 		//上下左右を反転。本番環境では、mat_frame1を反転させる
 		cv::flip(mat_frame2, mat_frame2, 0); //水平軸で反転（垂直反転）
 		cv::flip(mat_frame2, mat_frame2, 1); //垂直軸で反転（水平反転）
 		vconcat(mat_frame2, mat_frame1, dst_img_v);
 		
-		int iBrightness  = iSliderValue1 - 50;
-		double dContrast = iSliderValue2 / 50.0;
-		//mat_frame1.convertTo(dst_bright_cont, -1, dContrast, iBrightness); 
+		iBrightness  = iSliderValue1 - 50;
+		dContrast = iSliderValue2 / 50.0;
 		dst_img_v.convertTo(dst_bright_cont, -1, dContrast, iBrightness); //１枚にした画像をコンバート
 		//明るさ調整した結果を変換(Mat->IplImage*)して渡す。その後解放。
-		//*img_robot_side = dst_bright_cont;
 		*img_all_round = dst_bright_cont;
 		mat_frame1.release();
 		mat_frame2.release();
+		dst_img_v.release();
 		
-		//labeling for outer frame detection
-		IplImage *img_all_round_gray = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 1);
-		IplImage *dst_labelling_img = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
-		cvCvtColor(img_all_round, img_all_round_gray, CV_BGR2GRAY);
-		cv_Labelling(img_all_round_gray, dst_labelling_img);
-		cvReleaseImage (&img_all_round_gray);
-		/*cv::Mat eigen_img = cv::cvarrToMat(dst_labelling_img);  // データをMat形式でコピーする
-
-		cv::Mat gray_img;
-		cvtColor(eigen_img, gray_img, CV_BGR2GRAY);
-		normalize(gray_img, gray_img, 0, 255, cv::NORM_MINMAX);
-
-		std::vector<cv::Point2f> corners;
-		goodFeaturesToTrack(gray_img, corners, 80, 0.01, 5);
-		std::vector<cv::Point2f>::iterator it_corner = corners.begin();
-		for(; it_corner!=corners.end(); ++it_corner) {
-			if((it_corner->y < 76 || 345 < it_corner->y) && (it_corner->x < 75 || 690 < it_corner->x) ){
-				circle(eigen_img, cv::Point(it_corner->x, it_corner->y), 1, cv::Scalar(0,200,255), -1);
-				circle(eigen_img, cv::Point(it_corner->x, it_corner->y), 8, cv::Scalar(0,200,255));
-				std::cout << "x:" << it_corner->x << ", y:" << it_corner->y  << std::endl;
-			}
-		}*/
-		
-		// Init font
-		cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX|CV_FONT_ITALIC, 0.4,0.4,0,1);
 		IplImage* dst_img_mallett = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
-		IplImage* dst_img_pack = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
-		IplImage* dst_img2_mallett = cvCreateImage(cvGetSize(img2), IPL_DEPTH_8U, 3);
-		IplImage* dst_img2_pack = cvCreateImage(cvGetSize(img2), IPL_DEPTH_8U, 3);
+		//while前に宣言済み
+		//IplImage* dst_img_pack = cvCreateImage(cvGetSize(img_all_round), IPL_DEPTH_8U, 3);
+		IplImage* dst_img2_mallett = cvCreateImage(cvGetSize(img_all_round2), IPL_DEPTH_8U, 3);
+		IplImage* dst_img2_pack = cvCreateImage(cvGetSize(img_all_round2), IPL_DEPTH_8U, 3);
 
 		cv_ColorExtraction(img_all_round, dst_img_pack, CV_BGR2HSV, iSliderValuePack1, iSliderValuePack2, iSliderValuePack3, iSliderValuePack4, iSliderValuePack5, iSliderValuePack6);
 		cv_ColorExtraction(img_all_round, dst_img_mallett, CV_BGR2HSV, iSliderValueMallett1, iSliderValueMallett2, iSliderValueMallett3, iSliderValueMallett4, iSliderValueMallett5, iSliderValueMallett6);
-		cv_ColorExtraction(img2, dst_img2_pack, CV_BGR2HSV, iSliderValuePack1, iSliderValuePack2, iSliderValuePack3, iSliderValuePack4, iSliderValuePack5, iSliderValuePack6);
+		cv_ColorExtraction(img_all_round2, dst_img2_pack, CV_BGR2HSV, iSliderValuePack1, iSliderValuePack2, iSliderValuePack3, iSliderValuePack4, iSliderValuePack5, iSliderValuePack6);
 		
 		//CvMoments moment_mallett;
 		CvMoments moment_pack;
@@ -512,13 +543,6 @@ int main(int argc, char* argv[]) {
 		double b_intercept;
 		
 		int closest_frequency;
-		
-		//reacted limit-switch
-		if(gpioRead(13) == 1){
-			gpioWrite(25, 0);	
-			closest_frequency = gpioSetPWMfrequency(25, 0);
-			break;
-		}
 		
 		int target_coordinateX;
 		//pwm output for rotate
@@ -577,50 +601,79 @@ int main(int argc, char* argv[]) {
 		}
 
 		printf("target_coordinateX: %d\n",target_coordinateX);
-		
+
 		cvLine(show_img, cvPoint(CAM_PIX_WIDTH, target_destanceY), cvPoint(0, target_destanceY), cvScalar(255,255,0), 2);
 		cvLine(show_img, cvPoint((int)gX_now_mallett, (int)gY_now_mallett), cvPoint((int)target_coordinateX, target_destanceY), cvScalar(0,0,255), 2);
 		cvPutText (show_img, to_c_char((int)gX_now_mallett), cvPoint(460,30), &font, cvScalar(220,50,50));
 		cvPutText (show_img, to_c_char((int)target_coordinateX), cvPoint(560,30), &font, cvScalar(50,220,220));
 		
 		int amount_movement = gX_now_mallett - target_coordinateX;
-		//2枚の画像比較1回で移動できる量の計算
-		int max_amount_movement = CAM_PIX_WIDTH * 0.54 / 1; //比較にかかる時間:0.27*2, 端までの移動時間：1s
-		int target_direction;
-		if(amount_movement > 0){
-			if(max_amount_movement < amount_movement){
-			    amount_movement = max_amount_movement;
-			}
-			target_direction = 0;//時計回り
-		}
-		else if(amount_movement < 0){
-			amount_movement = -amount_movement;//正の数にする
-			if(max_amount_movement < amount_movement){
-				amount_movement = max_amount_movement;
-			}
+		int target_direction = -1;
+		//reacted limit-switch and target_direction rotate
+		if(gpioRead(6) == 1){//X軸右
+			gpioPWM(25, 128);	
+			closest_frequency = gpioSetPWMfrequency(25, 2000);
 			target_direction = 1;//反時計回り
+			printf("X軸右リミット！反時計回り\n");
 		}
-
-		//pwm output for delection
-		double set_time_millis= 270 * amount_movement / max_amount_movement;//0.27ms*(0~1)
-		gpioWrite(18, target_direction);
+		else if(gpioRead(26) == 1){//X軸左
+			gpioPWM(25, 128);
+			closest_frequency = gpioSetPWMfrequency(25, 2000);
+			target_direction = 0;//時計回り
+			printf("X軸左リミット！時計回り\n");
+		}
+		else if(gpioRead(5) == 1){//Y軸右上
+			gpioPWM(23, 128);
+			gpioSetPWMfrequency(23, 2000);
+			gpioWrite(14, 0);
+			printf("Y軸右上リミット！時計回り\n");
+		}
+		else if(gpioRead(13) == 1){//Y軸右下
+			gpioPWM(23, 128);	
+			gpioSetPWMfrequency(23, 2000);
+			gpioWrite(14, 1);
+			printf("Y軸右下リミット！反時計回り\n");
+		}
+		else if(gpioRead(19) == 1){//Y軸左下
+			gpioPWM(24, 128);	
+			gpioSetPWMfrequency(24, 2000);
+			gpioWrite(15, 0);
+			printf("Y軸左下リミット！時計回り\n");
+		}
 		
+		else if(gpioRead(21) == 1){//Y軸左上
+			gpioPWM(24, 0);
+			gpioSetPWMfrequency(24, 2000);
+			gpioWrite(15, 1);
+			printf("Y軸左上リミット！反時計回り\n");
+		}
+		else{
+			//Y軸固定のため
+			gpioSetPWMfrequency(23, 0);
+			gpioSetPWMfrequency(24, 0);
+			
+			if(amount_movement > 0){
+				target_direction = 0;//時計回り
+			}
+			else if(amount_movement < 0){
+				target_direction = 1;//反時計回り
+			}
+		}
+		if(target_direction != -1){
+			gpioWrite(18, target_direction);
+		}
 		printf("setting_frequency: %d\n", closest_frequency);
-		//gpioSetTimerFunc(0, (int)set_time_millis, pwmReset);
 
 		// 指定したウィンドウ内に画像を表示する
-		cvShowImage("Previous Image", img2);
+		cvShowImage("Previous Image", img_all_round2);
 		cvShowImage("Now Image", show_img);
 		cvShowImage("pack", dst_img_pack);
 		cvShowImage("mallett", dst_img_mallett);
-		cv::imshow("Vertical Concat", dst_img_v);
-		cvShowImage ("Outer Frame", dst_labelling_img);
 		
-		/*cvReleaseImage (&dst_img_mallett);
-		cvReleaseImage (&dst_img_pack);
+		cvReleaseImage (&dst_img_mallett);
+		//cvReleaseImage (&dst_img_pack);
 		cvReleaseImage (&dst_img2_mallett);
 		cvReleaseImage (&dst_img2_pack);
-		cvReleaseImage (&dst_labelling_img);*/
 		
         if(cv::waitKey(1) >= 0) {
             break;
@@ -630,22 +683,16 @@ int main(int argc, char* argv[]) {
     gpioTerminate();
     
     cvDestroyAllWindows();
-    //Clean up used cv::Mat
-    dst_bright_cont.release();
-	dst_img_v.release();
 	
 	//Clean up used CvCapture*
-	cvReleaseImage(&img_all_round);
 	cvReleaseCapture(&capture_robot_side);
 	cvReleaseCapture(&capture_human_side);
     //Clean up used images
+    cvReleaseImage(&img_all_round);
     cvReleaseImage(&img_human_side);
-    cvReleaseImage(&img2);
+    cvReleaseImage(&img_all_round2);
     cvReleaseImage(&show_img);
-    cvReleaseImage(&imgTracking);
     cvReleaseImage(&img_robot_side);
-	cvReleaseImage(&imgThresh);
-	cvReleaseImage(&imgThresh2);
 
     return 0;
 }
