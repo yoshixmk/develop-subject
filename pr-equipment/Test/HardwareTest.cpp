024b40acf23fad74162ae7388897781233e02eb9
YoshihiroUeki <pirori67@gmail.com>
2015-11-10 16:11:02 +0900

    TestCode methodtest added..

--- pr-equipment/Test/HardwareTest.cpp
+++ pr-equipment/Test/HardwareTest.cpp
@@ -6,5 +6,17 @@ namespace Test
 void HardwareTest::timerTest()
 {
 	std::cout << "!!!Timer Test!!!" << std::endl; // Timer Test start!
+
+	Hardware::Timer timer;
+	timer.setTimer(30);
+
+	//while(!timer.getAlarm()){
+		if(timer.getAlarm()){
+			std::cout << "getAlarm() retun Bool OK." << std::endl;
+		}
+		else{
+			std::cout << "getAlarm() retun Bool NG." << std::endl;
+		}
+	//}
 }
 }  // namespace HardwareTest
