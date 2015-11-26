66f9c5f2667348641d1a293ba4c45f8d48000578
YoshihiroUeki <pirori67@gmail.com>
2015-11-26 18:07:27 +0900

    FrameCoordinate RobotGoalLeft added.

--- pr-equipment/main.cpp
+++ pr-equipment/main.cpp
@@ -29,7 +29,7 @@ int main() {
 	//strategyTest.anomalyDetectionTest();
 //	strategyTest.packCoordinateTest();
 //	strategyTest.malletCoordinateTest();
-//	strategyTest.frameCoordinateTest();
+	strategyTest.frameCoordinateTest();
 //	strategyTest.locusTest();
 
 	Test::UITest uiTest;
@@ -38,6 +38,6 @@ int main() {
 
 	Test::ShowWindowTest showWindowTest;
 //	showWindowTest.windowTest();
-	showWindowTest.drawingTest();
+//	showWindowTest.drawingTest();
 	return 0;
 }
