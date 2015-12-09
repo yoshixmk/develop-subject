b32db76b01adaf1393df07fe9938753868d63a58
yoshixmk_school <pirori67@gmail.com>
2015-12-09 04:26:30 +0000

    pigpio serial ok.

---
+++ serial/serial_frequency.cpp
@@ -0,0 +1,66 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <pigpio.h>
+#include <iostream>
+#include <signal.h>
+
+/* シグナル受信/処理 */
+void signalHandler(int aSignal) {
+	std::cout << "SIGNAL Keybord Interrupt, END" << std::endl;
+	gpioTerminate();
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	//pwm initialize
+	//gpioCfgClock(4, 0, 100);
+	if (gpioInitialise() < 0) {
+		std::cout << "pigpio initialisation failed." << std::endl;
+	} else {
+		std::cout << "pigpio initialised okay." << std::endl;
+	}
+
+	/*if (signal(SIGINT, &signalHandler) == SIG_ERR) {
+		std::cout << "I could not set up signal. finished" << std::endl;
+		gpioTerminate();
+		exit(1);
+	}*/
+	
+	char sertty[] = "/dev/ttyAMA0";
+	int handle = serOpen(sertty, 19200, 0);
+	
+	if(handle>=0){
+		std::cout << "OK, serial port open" << std::endl;
+	}
+	else{
+		std::cout << "NG, serial port cannnot open" << std::endl;
+	}
+	
+	char input[5];
+	double start_time;
+	double now_time, passed_time;
+	
+	while(1){
+		
+		while(serDataAvailable(handle)){
+			start_time = time_time();
+			serRead(handle, input, 5);
+			std::cout << input << '\n' << std::flush;
+			//printf("%s\n" , input);
+			fflush(stdout);
+			now_time = time_time();
+			std::cout << now_time - start_time << '\n' << std::endl;
+			//printf("%f\n", now_time - start_time);
+			//fflush(stdout);
+		}
+		
+	}
+	serClose(handle);
+	gpioTerminate();
+
+	return 0;
+}
+
