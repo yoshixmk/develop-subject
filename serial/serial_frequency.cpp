41ebb9118b2f6d3478d8065aaf234b37edce3d33
yoshixmk_school <pirori67@gmail.com>
2015-12-14 10:42:20 +0000

    serial pr-equip test pass

--- serial/serial_frequency.cpp
+++ serial/serial_frequency.cpp
@@ -28,20 +28,18 @@ int main(int argc, char* argv[]) {
 	
 	//pigpio CW/CCW pin setup
 	//X:18, Y1:14, Y2:15
-	gpioSetMode(18, PI_OUTPUT);
+	gpioSetMode(25, PI_OUTPUT);
 	gpioSetMode(7, PI_OUTPUT);
-	gpioSetMode(12, PI_OUTPUT);
+	gpioSetMode(24, PI_OUTPUT);
 	//pigpio pulse setup
 	//X:25, Y1:23, Y2:24
-	gpioSetMode(25, PI_OUTPUT);
-	gpioSetMode(23, PI_OUTPUT);
-	gpioSetMode(24, PI_OUTPUT);
+	gpioSetMode(18, PI_OUTPUT);
+	gpioSetMode(19, PI_OUTPUT);
+	gpioSetMode(12, PI_OUTPUT);
 	
 	gpioWrite(6, 0);
 	
 	int f = gpioHardwarePWM(18, 0, 500000); 
-	
-	
 	
 	char sertty[] = "/dev/ttyAMA0";
 	int handle = serOpen(sertty, 19200, 0);
@@ -69,8 +67,8 @@ int main(int argc, char* argv[]) {
 			//暗黙のint変換。char->unsigned char->int
 			//0~255 -> 0~2550Hz とするための、10倍
 			if(isRead >= 0){
-				frequencyX = (unsigned char)input[0] * 10;
-				frequencyY = (unsigned char)input[1] * 10;
+				frequencyX = (unsigned char)input[0] * 10 * 2;
+				frequencyY = (unsigned char)input[1] * 10 * 2;
 				std::cout << "X: " << frequencyX << std::endl;
 				std::cout << "Y: " << frequencyY << std::endl;
 				
